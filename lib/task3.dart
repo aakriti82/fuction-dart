2729f16b1427fc014ed6b333c38921daef732dce
aakriti82 <aakritiyadav82@gmail.com>
2024-11-18 00:00:17 +0545

    task3

--- lib/task3.dart
+++ lib/task3.dart
@@ -1,10 +1,16 @@
 void main() {
-  int number = 5;
-  int factorial = 1;
-  
-  for (int i = 1; i <= number; i++) {
-    factorial *= i;
-  }
-  
-  print('Factorial of $number is $factorial');
+  int num1 = 10, num2 = 20, num3 = 15;
+  int largest = findLargest(num1, num2, num3);
+  print('The largest number is $largest');
 }
+
+int findLargest(int a, int b, int c) {
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
